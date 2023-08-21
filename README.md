de1872813104998db8c4dd2377a5c7cadeb4fd6d
Naman Rana <rananaman02@gmail.com>
2023-08-22 00:45:31 +0530

    Video chat app

---
+++ README.md
@@ -0,0 +1,14 @@
+# video-chat-app
+
+React js vide chat application is real time web application which allows users to video chat with their firends and colleagues
+
+check an live demo on https://hao-video-chat-app.netlify.app/
+
+
+# Tech stack used
+
+React js 
+Node js
+web socket API
+socket.io
+peer to peer
---
+++ index.js
@@ -0,0 +1,45 @@
+const app = require("express")();
+const server = require("http").createServer(app);
+const cors = require("cors");
+const path = require("path");
+
+const io = require("socket.io")(server, {
+	cors: {
+		origin: "*",
+		methods: [ "GET", "POST" ]
+	}
+});
+
+app.use(cors());
+
+const PORT = process.env.PORT || 5000;
+
+app.get('/', (req, res) => {
+	res.send('Running');
+});
+
+io.on("connection", (socket) => {
+	socket.emit("me", socket.id);
+
+	socket.on("disconnect", () => {
+		socket.broadcast.emit("callEnded")
+	});
+
+	socket.on("callUser", ({ userToCall, signalData, from, name }) => {
+		io.to(userToCall).emit("callUser", { signal: signalData, from, name });
+	});
+
+	socket.on("answerCall", (data) => {
+		io.to(data.to).emit("callAccepted", data.signal)
+	});
+});
+
+// if(process.env.NODE_ENV === "production"){
+// 	app.use(express.static('client/build'));
+
+//   app.get('*',(req,res)=>{
+// 	  res.sendFile(path.resolve(__dirname,'client','build','index.html')); 
+//   })
+// }
+
+server.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
